f8967696bbeb1cc8ac35dc76a9f1e9236bade2fc
ifdancoder <ifdancoder@gmail.com>
2021-07-24 13:15:36 +0800

    Added the Card class

--- BlackJack/BlackJack.cpp
+++ BlackJack/BlackJack.cpp
@@ -1,6 +1,28 @@
 ﻿#include <iostream>
+#include <vector>
 
 using namespace std;
+
+class Card {
+public:
+    enum suit { SPADES, HEARTS, CLUBS, DIAMONDS };
+    enum rnk { ACE = 1, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING };
+
+    Card(rnk rk = ACE, suit st = SPADES, bool ps = true) : cardRank(rk), cardSuit(st), cardPos(ps) {
+    }
+
+    void Flip() {
+        cardPos = !cardPos;
+    }
+
+    int GetValue() const {
+        return cardPos * (cardRank > 10 ? 10 : cardRank);
+    }
+protected:
+    bool cardPos;
+    suit cardSuit;
+    rnk cardRank;
+};
 
 int main()
 {
